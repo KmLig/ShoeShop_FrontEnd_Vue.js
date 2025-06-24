a2d9758b2da42bcd6990839fb2090934eb048ac1
Khiem <phamxuankhiem1102@gmail.com>
2025-06-24 23:56:13 +0700

    Add ShoeItem component

---
+++ frontend/src/components/ShoeItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg-white rounded-lg shadow-md overflow-hidden">
+        <img :src="shoe.image" :alt="shoe.name" class="w-full h-48 object-cover">
+        <div class="p-4">
+            <h3 class="text-lg font-medium text-gray-900">{{ shoe.name }}</h3>
+            <p class="text-gray-600">{{ shoe.description }}</p>
+            <p class="text-gray-600">{{ shoe.price }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Shoe } from '../types/Shoe';
+
+const props = defineProps<{
+    shoe: Shoe;
+}>();
+
+    
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/types/Shoes.ts
+++
@@ -1,7 +0,0 @@
-export interface Shoes {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
-    image: string | null;
-}
